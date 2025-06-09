95fd3e50b90410e79fdddcead3114492b4bdf101
tari2025 <denilestari272@gmail.com>
2025-06-09 10:36:52 +0700

    library iofilewithpath

--- contohExceptionHandling.cpp
+++ contohExceptionHandling.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    try
+    {
+        try
+        {
+            cout << "Selamat Belajar diProdi TI UMY" << endl;
+            throw 0.5; // melemparka sebuah integer maka
+            cout << "Pernyataan tidak akan di eksekusi" << endl;
+        }
+        catch (int a)
+        {
+            // blok ini akan di eksekusi
+            cout << " Pengecualian akan dieksekusi" << endl;
+        }
+    }
+}
