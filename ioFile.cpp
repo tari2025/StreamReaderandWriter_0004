f95311c174126104e89315f9f1f1a428ca7ff574
tari2025 <denilestari272@gmail.com>
2025-06-09 10:04:31 +0700

    operasi file dalammode menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+// steramreaderandWriter_nim
+
+int main()
+{
+    string baris;
+
+    // operasi filedalam mode menulis
+    ofstream outfile;
+    // menunjuk ke sebuah nama file untuk membuka
+    outfile.open("contohfile.txt");
+
+    cout << "<= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "- ";
+        // mendapatkan setiap kararakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")
+            break;
+    }
+}
